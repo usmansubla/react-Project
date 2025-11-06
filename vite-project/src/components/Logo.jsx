a125a708c800fed9a8f35f0c78474747cbf671f0
usmansubla <eausman745@gmail.com>
2025-11-06 21:23:11 +0530

    ddd

--- vite-project/src/components/Logo.jsx
+++ vite-project/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width="100px"}) {
   return (
     <div>
-      <img src="https://imgs.search.brave.com/1LgUcuHvx-D2fH1gaQARrkZOgvOrwkHSKYbrN0c3law/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTA0/MTk4NzQ4OC9waG90/by9jdXRlLWRvZy1w/dXQtaGlzLWZhY2Ut/b24taGlzLWtuZWVz/LXRvLXRoZS1tYW4t/YW5kLXNtaWxpbmct/ZnJvbS10aGUtaGFu/ZHMtc2NyYXRjaGlu/Zy1oZXItZWFyLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1O/S0dmOG5tWFZka3Nt/TlMwQXk2OTZjVlBO/U0lmQ0pKMXl1X3k5/akZHQnNNPQ" alt="" width="10px" />
+      <img src="https://imgs.search.brave.com/1LgUcuHvx-D2fH1gaQARrkZOgvOrwkHSKYbrN0c3law/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTA0/MTk4NzQ4OC9waG90/by9jdXRlLWRvZy1w/dXQtaGlzLWZhY2Ut/b24taGlzLWtuZWVz/LXRvLXRoZS1tYW4t/YW5kLXNtaWxpbmct/ZnJvbS10aGUtaGFu/ZHMtc2NyYXRjaGlu/Zy1oZXItZWFyLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1O/S0dmOG5tWFZka3Nt/TlMwQXk2OTZjVlBO/U0lmQ0pKMXl1X3k5/akZHQnNNPQ" alt="" />
     </div>
   )
 }
